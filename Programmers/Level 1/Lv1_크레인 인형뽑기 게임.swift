3f2bfe4a2b93820df486115b05741baf2908f96c
ryu-yeon <rytak108@gmail.com>
2022-01-04 06:43:37 +0900

    kakao

---
+++ Programmers/Level 1/Lv1_크레인 인형뽑기 게임.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
+    let lang = board.count
+    var newboard: [[Int]] = []
+    var pot: [Int] = []
+    var result = 0
+
+    for i in 0..<lang {
+        var arr: [Int] = []
+        for j in 0..<lang {
+            arr.append(board[j][i])
+        }
+        newboard.append(arr)
+    }
+
+    for x in moves {
+        for i in 0..<lang {
+            if newboard[x-1][i] != 0 {
+                if pot.count == 0 || pot[pot.count-1] != newboard[x-1][i] {
+                    pot.append(newboard[x-1][i])
+                    newboard[x-1][i] = 0
+
+                } else if pot.count > 0 && pot[pot.count-1] == newboard[x-1][i] {
+                    pot.remove(at: pot.count-1)
+                    newboard[x-1][i] = 0
+                    result += 2
+                }
+                break
+            }
+        }
+    }
+    return result
+}
