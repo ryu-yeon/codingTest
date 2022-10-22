d55bde7b3494ba3ea9bd4dd9bdb0d08094675ccc
ryu-yeon <rytak108@gmail.com>
2022-10-22 16:52:11 +0900

    update 왕실의 나이트.swift

--- 이것이 코딩테스트다/구현/왕실의 나이트.swift
+++ 이것이 코딩테스트다/구현/왕실의 나이트.swift
@@ -2,9 +2,7 @@ import Foundation
 
 let input = readLine()!.map{$0}
 
-let dic: [String: Int] = ["a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8]
-
-let X = dic[String(input[0])]!
+let X = Int(input[0].asciiValue! - Character("a").asciiValue! + 1)
 let Y = Int(String(input[1]))!
 
 let arrX: [Int] = [-2, -2, -1, -1, 1, 1, 2, 2]
